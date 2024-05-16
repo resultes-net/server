21b797a6edc6f183ce0bb0e362b763f8be034033
Damian Birchler <damian.birchler@ost.ch>
2024-05-16 18:00:58 +0200

    Also compute yearly total.

--- resultes_server/main.py
+++ resultes_server/main.py
@@ -21,8 +21,15 @@ async def create_file(
     df = pd.read_csv(bytes_io, sep="\t")
 
     total = df["Tot"]
-    monthly_total = total[:8761].groupby(months.get_month).sum()
+    monthly_total = total[:8761].groupby(months.get_month).sum() / 1000
 
-    monthly_values = monthly_total.to_dict()
+    monthly_values = {
+        "monthly_data": {
+            "months": monthly_total.index.tolist(),
+            "energy": monthly_total.values.tolist(),
+            "remark": "Energy in MWh.",
+        },
+        "yearly_data": {"total": monthly_total.sum()},
+    }
 
     return monthly_values
