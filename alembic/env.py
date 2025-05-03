d6e55989dc4b3f1823286d11c1cd8e8c2e8d470a
Damian Birchler <damian.birchler@ost.ch>
2025-05-03 17:31:46 +0200

    Upgrade DB schema using `alembic`.

--- alembic/env.py
+++ alembic/env.py
@@ -5,6 +5,11 @@ from sqlalchemy import pool
 
 from alembic import context
 
+from sqlmodel import SQLModel
+from resultes_server.models import *
+
+import resultes_server.config as server_config
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -18,7 +23,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = SQLModel.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
@@ -38,7 +43,7 @@ def run_migrations_offline() -> None:
     script output.
 
     """
-    url = config.get_main_option("sqlalchemy.url")
+    url = server_config.DB_HOST_NAME
     context.configure(
         url=url,
         target_metadata=target_metadata,
@@ -57,16 +62,22 @@ def run_migrations_online() -> None:
     and associate a connection with the context.
 
     """
+    configuration = config.get_section(config.config_ini_section, {})
+
+    print(
+        f"Overwriting URL {configuration["sqlalchemy.url"]} -> {server_config.DB_CONNECTION_STRING}."
+    )
+
+    configuration["sqlalchemy.url"] = server_config.DB_CONNECTION_STRING
+
     connectable = engine_from_config(
-        config.get_section(config.config_ini_section, {}),
+        configuration,
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
     )
 
     with connectable.connect() as connection:
-        context.configure(
-            connection=connection, target_metadata=target_metadata
-        )
+        context.configure(connection=connection, target_metadata=target_metadata)
 
         with context.begin_transaction():
             context.run_migrations()
