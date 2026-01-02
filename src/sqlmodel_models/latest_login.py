f973b9e30e2e11f3e0ef2127cab801d499e88130
Damian Birchler <damian.birchler@ost.ch>
2026-01-02 15:25:30 +0100

    Last login field requires  time zone.

--- src/sqlmodel_models/latest_login.py
+++ src/sqlmodel_models/latest_login.py
@@ -4,4 +4,6 @@ import resultes_pydantic_models.common as _pcom
 
 
 class LatestLogin(_sqlm.SQLModel, table=True):
-    on: _pcom.AwarePastDatetime = _sqlm.Field(primary_key=True)
+    on: _pcom.AwarePastDatetime = _sqlm.Field(
+        sa_column=_sqlm.Column(_sqlm.DateTime(timezone=True), primary_key=True)
+    )
